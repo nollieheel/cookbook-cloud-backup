0ea84237e77ae5dd45924b9958f12c43800ce002
nollieheel <iskitingbords@gmail.com>
2017-06-06 08:38:30 +0800

    Test notifying a compile-time resource

--- recipes/do_load.rb
+++ recipes/do_load.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Earth U (<iskitingbords @ gmail.com>)
 # Cookbook Name:: cookbook-cloud-backup
-# Recipe:: do_backup
+# Recipe:: do_load
 #
 # Copyright 2017, Earth U
 #
@@ -25,28 +25,33 @@ attribs = node[cookbook_name]
 dir_keys = "#{attribs['dir']['script']}/keys"
 
 directory(dir_keys) { recursive true }
-directory(attribs['dir']['log']) { recursive true }
+directory(attribs['dir']['tmp']) { recursive true }
 
 attribs['targets'].each do |t|
   tname = t[:id] || 'default'
 
-  is_backup = t.has_key?(:backup) ? t[:backup] : true
+  is_load = t.has_key?(:load) ? t[:load] : false
 
-  pub_path = if t[:encrypt_pub_key]
-    path_key = "#{dir_keys}/pub_#{tname.gsub(' ', '-')}.pem"
+  priv_path = if t[:encrypt_priv_key]
+    path_key = "#{dir_keys}/priv_#{tname.gsub(' ', '-')}.pem"
 
     file path_key do
-      content   t[:encrypt_pub_key]
+      content   t[:encrypt_priv_key]
       mode      0600
       owner     'root'
       group     'root'
       sensitive true
-      action    is_backup ? :create_if_missing : :delete
+      action    :nothing
+    end.run_action(is_load ? :create : :delete)
+
+    ruby_block 'notify_delete_priv_key' do
+      block {}
+      notifies :delete, "file[#{path_key}]", :delayed
     end
 
     path_key
-  elsif t[:encrypt_pub_path]
-    t[:encrypt_pub_path]
+  elsif t[:encrypt_priv_path]
+    t[:encrypt_priv_path]
   else
     nil
   end
@@ -55,14 +60,13 @@ attribs['targets'].each do |t|
     inst = Driver.const_get(drname).new(
       tname,
       paths: t[:paths],
-      backup_sched: t[:backup_sched],
-      backup_mailto: t[:backup_mailto],
       vars: dr,
       dir: attribs['dir'],
       bin: attribs['bin']
     )
 
-    inst.key_pub = pub_path
-    inst.sched_script(is_backup ? :create : :delete, run_context)
+    #inst.key_pub = pub_path
+    #inst.sched_script(is_backup ? :create : :delete, run_context)
   end
+
 end
